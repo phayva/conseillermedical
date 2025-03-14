6bac365cf4b620188c3601a2daacfaf073cc91c8
phayva Pablo <zoohayr@gmail.com>
2025-03-14 18:33:06 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 5  # Attendre 5 secondes pour laisser le bot et Flask s'initialiser
+sleep 10  # Attendre 5 secondes pour laisser le bot et Flask s'initialiser
 gunicorn main:app --bind 0.0.0.0:8000
