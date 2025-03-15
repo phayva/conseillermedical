09c5c6a6ae5483a4a0d15ad239451b595f845eba
phayva Pablo <zoohayr@gmail.com>
2025-03-15 13:27:02 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Démarrage de l'application avec Gunicorn..."
-sleep 10  # Attendre 10 secondes pour s'assurer que tout est prêt
+sleep 20  # Attendre 10 secondes pour s'assurer que tout est prêt
 exec gunicorn main:app --bind 0.0.0.0:8000 --log-level info --timeout 120
