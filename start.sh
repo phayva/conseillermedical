e7eab59023af64463f70c9d808b9b6a490f72151
phayva Pablo <zoohayr@gmail.com>
2025-03-15 11:27:43 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+echo "Démarrage de l'application avec Gunicorn..."
 sleep 10  # Attendre 5 secondes pour laisser le bot et Flask s'initialiser
-gunicorn main:app --bind 0.0.0.0:8000
+gunicorn main:app --bind 0.0.0.0:8000 --log-level info
