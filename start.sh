120b6a3f50e1812d71771237afa49f373658df0a
phayva Pablo <zoohayr@gmail.com>
2025-03-15 13:51:50 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Démarrage de l'application avec Gunicorn..."
-sleep 20  # Attendre 10 secondes pour s'assurer que tout est prêt
-exec gunicorn main:app --bind 0.0.0.0:8000 --log-level info --timeout 120
+sleep 20  # Délai augmenté pour donner le temps au bot de démarrer
+exec gunicorn main:app --bind 0.0.0.0:8000 --log-level info --timeout 120 --workers 2
